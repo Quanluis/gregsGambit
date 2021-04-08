95d346dcaaf6bfe00128df4493129b6eaaf76eb8
Luis Garzon <quanluis@protonmail.com>
2021-04-08 12:49:36 -0400

    added the necessary logic to the game, program displays the words the user inputs, counts points and displays them, nonwords included as well.

--- gregBoggle.js
+++ gregBoggle.js
@@ -21,14 +21,15 @@ function words(x){
 }
 
 
-function boggle(){
+function boggle()
+{
 
     var play = "";
     var score = 0; var flag = 0;
 
     var num = Math.floor(Math.random()*5 ) + 1; 
 
-    compWords = new Array(); notAWord = new Array();
+    compWords = new Array; notAWord = new Array;
 
     playWords = new Array();
 
@@ -40,34 +41,36 @@ function boggle(){
 
       // Get player entries
 
-    while(play != 'Q'){
+    while(play != 'Q')
+    {
         play = prompt('Enter a word or enter Q when done.');
         playWords.push(play);
-        if(play = 'Q'){
-            document.getElementById('entries').innerHTML = playWords.toString();
-        }
+        if(play != 'Q')
+            document.getElementById('entries').innerHTML = playWords.toString(); 
 
-
+    }
         //Check winning scores and list bad words
 
         var complgth = compWords.length;
 
         var playlgth = (playWords.length - 1);
 
-        for(var i = 0; i < playlgth; i++){
+        for(var i = 0; i < playlgth; i++)
+        {
             flag = 0;
-            for(var k = 0; k < complgth; k++){
-                if(playWords[i] ==  compWords[k]){
+            for(var k = 0; k < complgth; k++)
+            {
+                if(playWords[i] ==  compWords[k])
+                {
                     
                     score++;
                     flag = 1;
 
                 }
-                if(flag == 0)
-                    notAWord.push(playWords[i])
             }
-            document.getElementById('result').innerHTML = ("Your score is " + score + " . The following entries are not valid words: <br /> " +
-            notAWord.toString());
+            if(flag == 0)
+                notAWord.push(playWords[i]);         
         }
-    }      
-}
+        document.getElementById('result').innerHTML = ("Your score is " + score + " . The following entries are not valid words: <br /> " + notAWord.toString());
+    }   
+
